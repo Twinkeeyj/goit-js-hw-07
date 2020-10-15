58e5910e8546d8660e2dc406a8c84991c8c69cf4
Twinkeeyj <68866868+Twinkeeyj@users.noreply.github.com>
2020-10-15 23:40:04 +0300

    1

--- script.js
+++ script.js
@@ -12,5 +12,32 @@
 Категория: Животные
 Количество элементов: 4 */
 
-const titleSum = document.querySelector("ul#categories");
-console.log(titleSum);
+// const titleUl = document.querySelector('ul#categories');
+
+// let a = [1, 2, 3];
+// // let b = titleSum.children;
+// // titleSum.children;
+// const sum = array => array.children.reduce((acc, el, index) => acc + index, 0);
+// const b = array => array.children;
+// console.log(b);
+
+// const sum = array => {
+//   array.children.reduce((acc, el) => indexOf(acc) + indexOf(el));
+// };
+// const arr = [...array];
+//   return arr
+//     .sort((a, b) => a.friends.length - b.friends.length)
+//     .map(({ name }) => name);
+// };
+// const getUsersWithFriend = (array, friendName) =>
+//   array.filter(({ friends }) => friends.includes(friendName));
+
+const list = document.querySelector('ul#categories');
+console.log(`В списке ${list.children.length} категории.`);
+console.log(list);
+let title = document.querySelectorAll('ul#categories>li>h2');
+let titleList = [...title];
+console.log(titleList);
+const fn = arr =>
+  arr.forEach(el => console.log(`Категория: ${el.textContent}`));
+fn(titleList);
