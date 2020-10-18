5e2d13f289042729bed060f122cffaf83eb87fa5
Twinkeeyj <68866868+Twinkeeyj@users.noreply.github.com>
2020-10-18 16:26:29 +0300

    1

--- task-number_6.js
+++ task-number_6.js
@@ -15,35 +15,26 @@
 
 
 const inpute = document.querySelector("#validation-input");
-const data=inpute.getAttribute('data-length')
+const data=Number(  inpute.getAttribute('data-length'))
 
 console.log();
 
 inpute.addEventListener("change", (e) => {
-    // e.preventDefault();
-    
-console.log(inpute.value.length);
+
     if (inpute.value.length === 0) {
         inpute.classList.remove('valid')
-         inpute.classList.remove('invalid')
-    }
-    if (inpute.value.length >= data) {
         inpute.classList.remove('invalid')
-          inpute.classList.add('valid')
     }
-    if (inpute.value.length < data) {
-          inpute.classList.add('invalid')
-        inpute.classList.remove('valid')
+  
+    if (inpute.value.length === data) {
+        inpute.classList.remove('invalid')
+        inpute.classList.add('valid')
       
     }
-});
-// if (inpute.addEventListener("change", (e) => { }
-// )
-
-//     if (data < 6) {
-//     inpute.classList.remove('#validation-input')
-//     inpute.classList.add("#validation-input.invalid");
-//     } else if (data >= 6) {
-//         inpute.classList.remove("#validation-input.invalid")
-//     inpute.classList.add("#validation-input.valid");
-//   }
+    if (inpute.value.length > data || inpute.value.length < data) {
+        inpute.classList.remove('valid')
+        inpute.classList.add('invalid')
+    }
+    console.log(inpute.value.length);
+    console.log(data);
+})
